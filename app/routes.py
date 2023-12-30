15e33070681262c9e51a5c68485c6467e45b7694
Berry-More <gexanpropan@gmail.com>
2023-12-30 21:47:43 +0700

    Созданы post-функция для данных приходящих с прибора, get-функции для получения названий скважин и времен

---
+++ app/routes.py
@@ -0,0 +1,58 @@
+from app import app
+
+from db import dataBase
+from flask import redirect, url_for, jsonify, request
+
+"""
+Use 'request' package for work with API.
+
+Example: 
+
+params = {
+    'time_start': start_time, 
+    'time_end': end_time, 
+    'place': 'Kluchi'
+}
+
+response = requests.get(
+    'http://127.0.0.1:5000/temperature/times', 
+    params=params
+)
+"""
+
+
+@app.route('/temperature/data', methods=['GET', 'POST'])
+def data_access():
+    if request.method == 'GET':
+        args = request.args
+        print(args['name'])
+        return args
+    if request.method == 'POST':
+        if request.json:
+            new_data = {
+                'time': request.json['time'],
+                'depth': request.json['depth'],
+                'temp': request.json['temp'],
+                'place': request.json['places']
+            }
+            dataBase.post_data(new_data)
+            return 'created', 201
+
+
+@app.route('/temperature/places', methods=['GET'])
+def places_access():
+    try:
+        places = dataBase.get_places()
+        return jsonify({'places': places}), 200
+    except ValueError:
+        return jsonify({'places': None}), 200
+    except ConnectionError:
+        return jsonify({'places': None}), 522
+
+
+# test route
+# @app.route('/temperature/times', methods=['GET'])
+# def times_access():
+#     args = request.args
+#     times = dataBase.get_times(args['time_start'], args['time_end'], args['place'])
+#     return jsonify({'times': times}), 200
---
+++ db.py
@@ -0,0 +1,115 @@
+import psycopg2
+import numpy as np
+import pandas as pd
+
+from psycopg2 import sql
+from psycopg2.extras import DictCursor
+
+
+class WorkerDB:
+
+    """
+    Class for work with database
+    """
+
+    dbname = 'holedb'
+    user = 's_ponasenko'
+    password = 'Vdycm-8w3uZ'
+    host = 'localhost'
+    tab = 'holedb_schema.dts'
+
+    def post_data(self, data: dict):
+
+        """
+        :param data: dict with keys ['time', 'depth', 'temp', 'place']
+        :return: None
+
+        Post temperature data to the database
+        """
+
+        if len(data['depth']) > 0:
+            values = []
+            for i in range(len(data['depth'])):
+                values.append(
+                    (data['time'],
+                     data['depth'][i],
+                     data['temp'][i],
+                     data['place'])
+                )
+
+            with psycopg2.connect(dbname=self.dbname, user=self.user, password=self.password, host=self.host) as conn:
+                with conn.cursor(cursor_factory=DictCursor) as cursor:
+                    insert = sql.SQL('''
+                    INSERT INTO {} (time, depth, temp, place) 
+                    VALUES '''.format(self.tab) + '{}').format(sql.SQL(',').join(map(sql.Literal, values)))
+                    cursor.execute(insert)
+
+    def get_places(self) -> list[str]:
+
+        """
+        :return: list
+
+        Get all wells names there are in database
+        """
+
+        try:
+            with psycopg2.connect(dbname=self.dbname, user=self.user, password=self.password, host=self.host) as conn:
+                with conn.cursor(cursor_factory=DictCursor) as cursor:
+                    select = """
+                    SELECT DISTINCT place
+                    FROM {0}
+                    """.format(self.tab)
+                    cursor.execute(select)
+                    result = cursor.fetchall()
+
+                    if len(result) == 0:
+                        raise ValueError('No wells available')
+                    else:
+                        return sum(result, [])
+
+        except psycopg2.OperationalError:
+            raise ConnectionError('Unable to connect to database')
+
+    def get_depth_range(self, time_start: float, time_end: float, place: str) -> tuple[float, float]:
+        try:
+            with psycopg2.connect(dbname=self.dbname, user=self.user, password=self.password, host=self.host) as conn:
+                with conn.cursor(cursor_factory=DictCursor) as cursor:
+                    select = """
+                    SELECT DISTINCT time
+                    FROM {0}
+                    WHERE time > {1} AND time < {2} AND place = '{3}';
+                    """
+
+        except psycopg2.OperationalError:
+            raise ConnectionError('Unable to connect to database')
+
+    def get_times(self, time_start: float, time_end: float, place: str) -> np.array:
+
+        """
+        :param time_start: float (timestamp)
+        :param time_end: float (timestamp)
+        :param place: str
+        :return: np.array
+        """
+
+        try:
+            with psycopg2.connect(dbname=self.dbname, user=self.user, password=self.password, host=self.host) as conn:
+                with conn.cursor(cursor_factory=DictCursor) as cursor:
+                    select = """
+                    SELECT DISTINCT time
+                    FROM {0}
+                    WHERE time > {1} AND time < {2} AND place = '{3}';
+                    """.format(self.tab, time_start, time_end, place)
+                    cursor.execute(select)
+                    result = cursor.fetchall()
+
+                    if len(result) == 0:
+                        raise ValueError('No data for the selected time period')
+                    else:
+                        return np.array(result).T[0]
+
+        except psycopg2.OperationalError:
+            raise ConnectionError('Unable to connect to database')
+
+
+dataBase = WorkerDB()
